2b0f30dbb2bcf2378b2d7502f389af27c0e86380
Lorenzo Amata <lorenzoamata@gmail.com>
2021-10-06 19:49:54 +0200

    First commit, set cardlayout, login panel (card1), connect function to enstablish connection & the login button action

---
+++ src/ApplicationDemo.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+public class ApplicationDemo {
+    private JPanel panellogin;
+    private JPanel panellomain;
+    private JPanel panellouser;
+    private JPanel panelloproduct;
+    private JPanel panellocard;
+    private JLabel logolabel;
+    private JPanel Main;
+    private JTextField usernamefield;
+    private JPasswordField passwordfield;
+    private JButton loginButton;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("CRUD_O");
+        frame.setContentPane(new ApplicationDemo().Main);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+    Connection con;
+    PreparedStatement pst;
+
+    public boolean connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/enterprisedb", "root", "");
+            System.out.println("Successfully connected via jdbc Driver to the server    \n");
+            return true;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            return false;
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+
+    public ApplicationDemo(){
+        //set enterprise icon
+        ImageIcon iconlogo = new ImageIcon("logo.png");
+        logolabel.setIcon(iconlogo);
+        connect();
+
+        //action listener
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //TODO: manage hash function in db
+                try {
+                    pst = con.prepareStatement("select id from user where username=? and password=?");
+                    pst.setString(1,usernamefield.getText());
+                    pst.setString(2, String.valueOf(passwordfield.getPassword()));
+                    ResultSet rs = pst.executeQuery();
+                    if(rs.next()){
+                        JOptionPane.showMessageDialog(null,"Login Successful");
+                    }
+                    else{
+                        //TODO: manage and divide the two options ( user or password or both)
+                        JOptionPane.showMessageDialog(null,"Password or User incorrect");
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+    }
+
+}
